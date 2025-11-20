f9064afca9794c7b5664819a8073bc0fe4c3168b
Binsadboiiz <ngnphcng@gmail.com>
2025-11-20 23:38:41 +0700

    Register Form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import React from "react";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 
 
 
 function App() {
-  const handleLoginSuccess = (user) => {
-    alert("Login successful", user);
-  }
-  return <LoginForm onLoginSuccess={handleLoginSuccess}/>
+  return (
+      <Routes>
+        <Route path='/' element={<LoginForm/>}/>
+        <Route path='/register' element={<RegisterForm/>}/>
+        <Route path='/login' element={<LoginForm/>}/>
+      </Routes>
+  )
 }
 
 export default App;
--- frontend/src/components/loginForm.jsx
+++ frontend/src/components/loginForm.jsx
@@ -1,47 +1,81 @@
-import  React , { useState } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../styles/loginform.css";
 
-export default function LoginForm({onLoginSuccess}) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(false);
+export default function LoginForm() {
+  const [formData, setFormData] = useState({
+    usernameOrEmail: "",
+    password: ""
+  });
+  const [successMsg, setSuccessMsg] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData(prev=>({...prev, [name]: value}));
+  }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError("");
-        setLoading(true);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccessMsg("");
 
-        try {
-            const res = await fetch("http://localhost:3000/api/users/login", {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({username, password})
-            })
+    try {
+      const res = await fetch("http://localhost:5000/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
 
-            const data = await res.json();
-            if(!res.ok) {throw new Error(data.message || "Something went wrong");}
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data.message || "Something went wrong");
+      }
 
-            if(onLoginSuccess) {
-                onLoginSuccess(data.user);
-            }
-        } catch (err) {
-            setError(err.message);
-            setLoading(false);
-        }
-    };
+      setSuccessMsg(data.message || "Đăng nhập thành công")
+      
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return(
-        <div className="login-container">
-            <form action="" onSubmit={handleSubmit} className="login-form">
-                <h2>Longin</h2>
-                <label htmlFor="username">Username or Email:</label>
-                <input type="text" name="username" id="username" value={username} autoComplete="username" onChange={(e)=>setUsername(e.target.value)} placeholder="Enter your username or email"/>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" autoComplete="current-password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter your password"/>
-                <button type="submit" disabled={loading}>{loading ? "Loading..." : "Login"}</button>
-            </form>
+  return (
+    <div className="login-container">
+      {error && <div className="error-box">{error}</div>}
+      {successMsg && <div className="success-box">{successMsg}</div>}
+      <form action="" onSubmit={handleSubmit} className="login-form">
+        <h2>Longin</h2>
+        <label htmlFor="username">Username or Email:</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={formData.username}
+          autoComplete="username"
+          onChange={handleChange}
+          placeholder="Enter your username or email"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          autoComplete="current-password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Enter your password"
+        />
+        <button type="submit" disabled={loading}>
+          {loading ? "Loading..." : "Login"}
+        </button>
+
+        <div className="switch-auth">
+          Chưa có tài khoản? <Link to="/register">Đăng ký ngay</Link>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
