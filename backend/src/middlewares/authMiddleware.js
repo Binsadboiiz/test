0d8e611d79368aa92981255f5682b0e9bb10bee8
Binsadboiiz <ngnphcng@gmail.com>
2025-11-24 23:55:03 +0700

    fix get token/cookie

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -6,25 +6,25 @@ export default function authMiddleware(req, res, next) {
     try {
         let token;
 
-        const authHeader = req.headers.authorization;
-
-        if(authHeader && authHeader.startsWith("Bearer")) {
-            token = authHeader.split("")[1];
+        if(req.cookie && req.cookie.token) {
+            token = req.cookie.token;
         }
 
         if(!token) {
-            throw new ErrorApi("Missing authentication token", 401);
+            const authHeader = req.headers.authorization;
+            if(authHeader && authHeader.startsWith("Bearer")) {
+                token = authHeader.split(" ")[1]
+            }
         }
 
-        const secret = process.env.JWT_SECRET || "dev_secret_change_me";
-        const decoded = jwt.verify(token, secret);
-
+        if(!token) throw new ErrorApi("Missing authentication token", 401);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = {
             _id: decoded._id,
             username: decoded.username,
             displayname: decoded.displayname,
-            roles: decoded.roles || []
-        };
+            roles: decoded.roles
+        }
         next();
     } catch (error) {
         if(error.name === "JsonWebTokenError" || error.name === "TokenExpiredError")
